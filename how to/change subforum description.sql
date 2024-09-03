eb55343430a2a413cba30befd702099ac084866b
Hunter Browning <chanbot1116@gmail.com>
2024-09-02 22:51:36 -0700

    Update change subforum description.sql

--- how to/change subforum description.sql
+++ how to/change subforum description.sql
@@ -1,4 +1,44 @@
 UPDATE [MyAdventureGame].[dbo].[Forums_Forums]
-   SET [Description] = 'DESC'
- WHERE [Name] = 'FORUM NAME'
+   SET [Description] = 'Introduce yourself and get to know the community.'
+ WHERE [Name] = 'Newbie Central'
+GO
+UPDATE [MyAdventureGame].[dbo].[Forums_Forums]
+   SET [Description] = 'A place to sit back, hang out, and make monkey noises about anything you''d like.'
+ WHERE [Name] = 'The Lounge'
+GO
+UPDATE [MyAdventureGame].[dbo].[Forums_Forums]
+   SET [Description] = 'Keep up with the latest contests and find out what''s been changing with the site.'
+ WHERE [Name] = 'News & Updates'
+GO
+UPDATE [MyAdventureGame].[dbo].[Forums_Forums]
+   SET [Description] = 'Questions about a storygame? Thoughts on Eternal? Any other IF you''re playing out there?'
+ WHERE [Name] = 'The Parlor Room'
+GO
+UPDATE [MyAdventureGame].[dbo].[Forums_Forums]
+   SET [Description] = 'Take part in collaborative works, share your short stories, poems, original artwork and more.'
+ WHERE [Name] = 'Creative Corner'
+GO
+UPDATE [MyAdventureGame].[dbo].[Forums_Forums]
+   SET [Description] = 'Find proofreaders here, useful resources, and share opinions and advice on story crafting.'
+ WHERE [Name] = 'Writing Workshop'
+GO
+UPDATE [MyAdventureGame].[dbo].[Forums_Forums]
+   SET [Description] = 'Did you know books used to be made from trees? Dust off a seat and discuss your reading escapades here...you do read, right?'
+ WHERE [Name] = 'Reading Corner'
+GO
+UPDATE [MyAdventureGame].[dbo].[Forums_Forums]
+   SET [Description] = 'Get help from the experts on variables, scripts, items, and other scary things.'
+ WHERE [Name] = 'Advanced Editor Forum'
+GO
+UPDATE [MyAdventureGame].[dbo].[Forums_Forums]
+   SET [Description] = 'Suggestions for improvements and additions to the site.'
+ WHERE [Name] = 'Feature Wishing Well'
+GO
+UPDATE [MyAdventureGame].[dbo].[Forums_Forums]
+   SET [Description] = 'Post reports about problems or bugs in here.'
+ WHERE [Name] = 'Bugs & Problems'
+GO
+UPDATE [MyAdventureGame].[dbo].[Forums_Forums]
+   SET [Description] = 'For moderator eyes only.'
+ WHERE [Name] = 'Moderator Central'
 GO
